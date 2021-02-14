47e44bde095fed978bf133c9a6871ac694b3c16f
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2021-02-14 16:41:42 +0500

    Project Setup for Big Kahuna Burger

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <div>
+       <Layout>
+         This is Burger Builder
+       </Layout>
+    </div>
+  );
+}
+
+export default App;
